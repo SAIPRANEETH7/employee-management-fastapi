2de89ce07ee9807c4b4e0ada2efa8f833d4f2d60
Sai Praneeth <avadhanampraneeth@gmail.com>
2026-09-13 11:41:43 +0530

    change: main

--- app/main.py
+++ app/main.py
@@ -128,4 +128,4 @@ def delete_existing_employee(
             detail="Employee not found.",
         )
 
-    return {"message": "Employee deleted successfullyes."}
+    return {"message": "Employee deleted successfully."}
