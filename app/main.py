e7daf3a1784b4a21855baf6963983c99d885c971
Sai Praneeth <avadhanampraneeth@gmail.com>
2026-09-16 09:52:01 +0530

    change 6

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 @app.get("/health")
 def health_check():
-    return {"status": "Application is running"}
+    return {"status": "Application is running successfully."}
 
 
 @app.post(
